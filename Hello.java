495cd0654548b627e581b3d7ed76a786146f7653
unknown <montasirazad@gmail.com>
2023-07-17 03:17:09 +0600

    hello world from java 2

--- Hello.java
+++ Hello.java
@@ -5,3 +5,12 @@ class Hello {
         System.out.println("Hello world");
     }
 }
+
+
+class Hello2 {
+    public static void main(String a[])
+
+    {
+        System.out.println("Hello world 2");
+    }
+}
