8c54b7f529e1583b4a73907852a3fe13ae6395d3
unknown <montasirazad@gmail.com>
2023-07-17 03:19:34 +0600

    new comnit

--- Hello.java
+++ Hello.java
@@ -3,14 +3,8 @@ class Hello {
 
     {
         System.out.println("Hello world");
-    }
-}
-
-
-class Hello2 {
-    public static void main(String a[])
-
-    {
         System.out.println("Hello world 2");
     }
 }
+
+ 
